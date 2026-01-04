c76617bbba336ba0fe0615fb25d20947c94ec632
Ifiok Ekott <ekottifiok@gmail.com>
2026-01-04 19:35:56 +0100

    new cool_feature

--- src/lib/httpResponses.ts
+++ src/lib/httpResponses.ts
@@ -1,4 +1,6 @@
-
+export function tester(status:number, res: any) {
+  return res.status(status).json({message: 'hello'})
+}
 
 
 export function httpResponse(status: number, message: string, data: any, res: any) {
@@ -7,7 +9,7 @@ export function httpResponse(status: number, message: string, data: any, res: an
     message,
     data,
   });
-} 
+}
 
 export function wpApiHttpRespone(status: number,data: any, res: any) {
   return res.status(status).json(data);
